dbf4fbe185a24936adbd0238b57bb279b5eafd5b
janardhan-boddeti <68552900+janardhan-boddeti@users.noreply.github.com>
2020-07-21 20:11:31 +0530

    Add files via upload

---
+++ LetusUpgrade Python Essentials- Day 2(21-07-2020).py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("Hello\ Python")
+
+
+# In[6]:
+
+
+print("hello\a")
+
+
+# In[7]:
+
+
+print('Python\'s world')
+
+
+# In[8]:
+
+
+print("Python's world")
+
+
+# In[9]:
+
+
+name="XYZ"
+marks=90.867
+age=20
+print("The Name of person is ", name, "marks is ",marks, "age is",age)
+
+
+# In[11]:
+
+
+print("The Name of person is %10s marks is %10.2f age is %10d"%(name,marks,age))
+
+
+# In[12]:
+
+
+print("The Name of person is %10s marks is %10.1f age is %10d"%(name,marks,age))
+
+
+# In[13]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[14]:
+
+
+c=10
+a==c
+
+
+# In[15]:
+
+
+c<=a
+
+
+# In[16]:
+
+
+a>=b
+
+
+# In[19]:
+
+
+a = get_ipython().getoutput('b')
+
+
+# In[20]:
+
+
+str1="pythonprogramming"
+"a" in str1
+
+
+# In[21]:
+
+
+str1="pythonprogramming"
+"x" not in str1
+
+
+# In[ ]:
+
+
+
+
