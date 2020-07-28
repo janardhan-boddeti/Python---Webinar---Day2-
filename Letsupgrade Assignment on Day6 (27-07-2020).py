125b82ce42ccbd298c573c482f402c73ed70c7f9
janardhan-boddeti <68552900+janardhan-boddeti@users.noreply.github.com>
2020-07-28 10:49:21 +0530

    Add files via upload

---
+++ Letsupgrade Assignment on Day6 (27-07-2020).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+keys=[1,2,3,4,5,7,8]
+values=["a","b","c","d","e"]
+
+res={ keys[i]:values[i] for i in range(0,5)}
+print("dict: "+str(res))
+
+
+# In[ ]:
+
+
+
+
